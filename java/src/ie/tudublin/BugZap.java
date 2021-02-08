0af4ebfd1fa39f6c2b7249508e1c0263d8da56a3
jadehiggins00 <jadehiggins2000@gmail.com>
2021-02-08 21:54:18 +0000

    partially complete

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -4,54 +4,57 @@ import processing.core.PApplet;
 
 // class name has to match the file name
 public class BugZap extends PApplet {
-    
-	public void settings(){
 
-        size(500,500);
+	public void settings() {
 
-    }//end method
+		size(500, 500);
+
+	}// end method
 
 	public void setup() {
-        //calling the reset method
+		// calling the reset method
 		reset();
-    }//end method
+	}// end method
 
-    float playerX;
-    float playerY;
-    float playerWidth = 40;
+	float playerX;
+	float playerY;
+	float playerSpeed = 5;
+	float playerWidth = 40;
 	float halfPlayerWidth = playerWidth / 2;
-    
-	//making variable for the bug
+
+	// making variable for the bug
 	float bugX, bugY, bugWidth = 30;
 	float halfBugWidth = playerWidth / 2;
 
-	//score var
+	// score var
 	int score = 0;
 
 	// this method resets the bug and assigns the player to the starting position
 	void reset() {
 		resetBug();
-		// these var dont get assigned values until the size() is called 
-		playerX = width /2;
-		//this means it is 50 pixels up from the screen
+		// these var dont get assigned values until the size() is called
+		playerX = width / 2;
+		// this means it is 50 pixels up from the screen
 		playerY = height - 50;
 
-	}//end method
+	}// end method
 
-	// everytime the playe shoots the bug, the user wants to assign it back to its starting position
+	// everytime the playe shoots the bug, the user wants to assign it back to its
+	// starting position
 	void resetBug() {
-		// want to start the bug at least half of the bug into the screen ( so that it appears on screen) 
+		// want to start the bug at least half of the bug into the screen ( so that it
+		// appears on screen)
 		// and to minus the halfbugwidth from the width for the right side of the screen
 		bugX = random(halfBugWidth, width - halfBugWidth);
 		// width is equalled to 50px
 		bugY = 50;
 
-	}//end method
+	}// end method
 
-   // draw the bug
-   void drawBug(float x, float y){
+	// draw the bug
+	void drawBug(float x, float y) {
 
-		//draw the bug
+		// draw the bug
 		// this is used to add colour to the outline of the shape or line
 		stroke(255); // 255 is white
 		float saucerHeight = bugWidth * 0.7f; // var for the saucer
@@ -59,24 +62,22 @@ public class BugZap extends PApplet {
 		line(x, y - saucerHeight, x - halfBugWidth, y);
 		// this draws the line at the RIGHT SIDE of the bug
 		line(x, y - saucerHeight, x + halfBugWidth, y);
-		//line(x - halfBugWidth, y, x - halfBugWidth, y);
+		// line(x - halfBugWidth, y, x - halfBugWidth, y);
 		// this draws the line at the bottom of the triangle
 		line(x - halfBugWidth, y, x + halfBugWidth, y);
 		// drawing the two feet for the bug
 		float feet = bugWidth * 0.1f;
-		//two feet
+		// two feet
 		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
 		line(x + feet, y, x + halfBugWidth, y + halfBugWidth);
 
-		//drawing the eyes
+		// drawing the eyes
 		float eyes = bugWidth * 1.0f;
 		line(x - eyes, y - eyes, x - eyes, y - eyes * 2f);
 		line(x + eyes, y - eyes, x + eyes, y - eyes * 2f);
-   }//end method
+	}// end method
 
-
-
-    public void drawPlayer(float x, float y, float w){
+	public void drawPlayer(float x, float y, float w) {
 		stroke(255);
 		// drawing the player
 		float playerHeight = w / 2;
@@ -92,49 +93,85 @@ public class BugZap extends PApplet {
 
 		line(x, y, x, y + playerHeight * 0.3f);
 
-    }//end method
+	}// end method
 
-    // public void playerLaser(float x, float y, float w){
-    //     fill(255, 255, 255);
-    //     line (200, 300, 100, 50);
+	// public void playerLaser(float x, float y, float w){
+	// fill(255, 255, 255);
+	// line (200, 300, 100, 50);
 
-    // }//end mehtod
-    
-    public void draw(){
+	// }//end mehtod
 
-        background(0); 
+	public void draw() {
 
-        noStroke();
+		background(0);
+		fill(255);
+		// calling the method and drawing the player
+		drawPlayer(playerX, playerY, playerWidth);
+		// calling the method and drawing the bug
+		drawBug(bugX, bugY);
+		moveBug();
+		text("SCORE " + score, 20, 20);
 
-        drawPlayer(playerX,playerY,playerWidth);
-       
+	}// end method
 
-    }//end method
+	void moveBug() {
 
-    public void keyPressed()
-	{
-		//left arrow
-		if (keyCode == LEFT)
-		{
-			// if the player reaches the side of the screen  nad makes sure it doesnt go off the screen
-			if (playerX > halfPlayerWidth){
-            	//decremeting the variable so that it moves left
-            	playerX -= playerSpeed;
+		// frame count var - every half a second
+		if ((frameCount % 30) == 0) {
+
+			// making the bug move at random from left to right
+			bugX += random(-5, 5); // random number from -5 to +5
+
+			// bounce checking on the X so it doesnt bounce off the screen
+			if (bugX < halfBugWidth) { // stops from going off the screen LEFT SIDE
+				bugX = halfBugWidth;
+			} // end if
+
+			if (bugX > width - halfBugWidth) { // RIGHT SIDE
+				bugX = width - halfBugWidth;
+			}
+			bugY += 2;
+		} // end if
+	}// end mehtod
+
+	public void keyPressed() {
+		// left arrow
+		if (keyCode == LEFT) {
+			// if the player reaches the side of the screen nad makes sure it doesnt go off
+			// the screen
+			if (playerX > halfPlayerWidth) {
+				// decremeting the variable so that it moves left
+				playerX -= playerSpeed;
 				System.out.println("Left arrow pressed");
 
-			}//end inner if
+			} // end inner if
 		}
-		if (keyCode == RIGHT)
-		{
-            //incremtint the playerx variable so that it moves right
-            playerX ++;
-			System.out.println("Right arrow pressed");
+		if (keyCode == RIGHT) {
+			// if its okay to move if i havent reached the roight side
+			if (playerX < width - halfPlayerWidth) {
+				// incremtint the playerx variable so that it moves right
+				playerX += playerSpeed;
+				System.out.println("Right arrow pressed");
+			} // end inner if
+
 		}
-		if (key == ' ')
-		{
-            line(playerX, playerY, playerX, 0);
-            
+		if (key == ' ') {
+			// if the playerX is greater than the left side of the bug and less than bugX plus halfbugwidth
+			if(playerX > bugX - halfBugWidth && playerX < bugX +  halfBugWidth){
+				// draw a line that goes from the player and hits the bug
+				line(playerX,playerY, playerX, bugY);
+				//increase the score
+				score ++;
+				resetBug();
+			}//end if 
+			
+			else {
+				line(playerX, playerY, playerX, 0);
+			}//end else
+
 			System.out.println("SPACE key pressed");
 		}
+
+		
 	}
-}//end class
+}// end class
