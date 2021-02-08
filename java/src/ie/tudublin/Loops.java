f7f451de467a27552bb123e30d1cbd5d1ec97146
jadehiggins00 <jadehiggins2000@gmail.com>
2021-02-08 19:28:36 +0000

    another version

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -6,12 +6,13 @@ public class Loops extends PApplet {
 
     public void settings() {
         size(500, 500);
+        // two useful vars we are going to use in our ptrogram 
         cx = width / 2;
         cy = height / 2;        
     }
 
     int mode = 0;
-
+    
     float cx;
     float cy;
 
@@ -45,6 +46,50 @@ public class Loops extends PApplet {
 
                 // ellipse(cx, cy, 100, 100);
                 // break;
-        }
-    }
-}
+
+            case 1:{
+
+                
+
+                if(mouseX > cx){
+                    rect(0,0,cx,height/2);
+
+                }//end if
+
+                else{
+                    rect(cx,cy,cx,cy);
+                }
+
+            }//end case1
+                break;
+            case 2: {
+                // int numRects = 10;
+                int numRects =(int)(mouseX/10.0f); //colours move when mouse moves2
+                float w = width /(float) numRects;
+                float cgap = 255/ (float) numRects;
+
+                for(int i= 0 ; i < numRects; i ++){
+                    fill(i * cgap, 255,255);
+                    rect(i * w,0,w,height);
+                }//end for loop
+            }//end case
+            break;
+
+            case 3:{
+                
+                int numCircles = (int)(mouseX/ 10.0f);
+                float w = width/(float) numCircles;
+                float cgap = 255/(float) numCircles; //cgap = colour gap
+                    
+                for(int i= 0 ; i < numCircles; i ++){
+
+                    fill(i * cgap, 255,255);      
+                    // ellipse(w/2 + i * w);
+                    // ellipse(w/2 + i * w);
+                }//end for loop
+            }//end case 3
+        }//end switch
+      
+          
+    }//end draw method
+}//end class
