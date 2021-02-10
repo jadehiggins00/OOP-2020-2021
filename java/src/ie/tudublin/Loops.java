a1f7010d35eb2899c333f46656c477135dfeac1c
jadehiggins00 <jadehiggins2000@gmail.com>
2021-02-10 20:59:51 +0000

    another commit

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -49,19 +49,30 @@ public class Loops extends PApplet {
 
             case 1:{
 
-                
-
-                if(mouseX > cx){
-                    rect(0,0,cx,height/2);
+                fill(50,255,255);
+                // top quadrant
+                if(mouseX < cx && mouseY < cy){
+                    //cx and cy is the width of the rectangle
+                    rect(0,0,cx,cy);
 
                 }//end if
+                
+                // top right quadrant
+                else if (mouseX > cx && mouseY < cy  ){
+                    rect(cx,0, cx, cy);
+                }//end else if
+
+                else if ( mouseX < cx && mouseY > cy){
+                    rect(0, cy, cx, cy);
+                }//end else if
 
                 else{
-                    rect(cx,cy,cx,cy);
-                }
+                    rect(cx, cy, cx, cy);
+                }//end else
 
             }//end case1
-                break;
+            break;
+
             case 2: {
                 // int numRects = 10;
                 int numRects =(int)(mouseX/10.0f); //colours move when mouse moves2
@@ -78,26 +89,28 @@ public class Loops extends PApplet {
             case 3:{
                 
                 int numCircles = (int)(mouseX/ 10.0f);
+                // w is the width of each individual circ;e
                 float w = width/(float) numCircles;
-                float cgap = 255/(float) numCircles; //cgap = colour gap
+                float cgap = 255/(float) numCircles; //cgap = colour gap - rainbow colour
                     
                 for(int i= 0 ; i < numCircles; i ++){
 
-                    fill(i * cgap, 255,255);      
-                    ellipse(56,46,55,55);
+                    fill(i * cgap, 255,255);   
+                    // w/2 because you have to start with haldf of the circle
+                    ellipse(w / 2 + (i * w), cy, w, w);
                 
                 }//end for loop
             }//end case 3
             break;
             case 4: {
                
-                int numLines = 7;
+                int numLines = 10;
                 float theta = TWO_PI / (float) numLines; // angle bwteen the five lines
                 float radius = 150;
                 for ( int i=0 ; i < numLines; i++){
 
                     float angle = theta * i;
-                    float x = cos(angle) * cos(angle);
+                    float x = cos(angle) * radius;
                     float y = sin(angle) * radius;
                     stroke(255);
                     line(cx,cy,cx +x, cy+y);
@@ -106,6 +119,30 @@ public class Loops extends PApplet {
 
             }//end case 4
             break;
+
+            case 5: {
+
+                int numSquares = (int) (mouseX / 2.0f);
+                float theta = TWO_PI / (float) numSquares; // angle bwteen the five lines
+                float cgap = 255 / (float) numSquares;
+
+                for(int i= 0 ; i < numSquares; i ++){
+
+                    float angle = theta * i;
+                    float x = cos(angle) * numSquares;
+                    float y = sin(angle) * numSquares;
+
+                    fill(i * cgap, 255,255);   
+                    // w/2 because you have to start with haldf of the circle
+                    rect(cx,cy,cx + x,cy+y);
+                
+                }//end for loop
+
+
+
+            }//end case 5
+            break;
+
             default:{
 
             }
