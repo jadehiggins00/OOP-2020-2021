39c457bcf3f0281a27ce935e624bc06709a5ac8a
jadehiggins00 <jadehiggins2000@gmail.com>
2021-02-15 12:44:28 +0000

    first commit

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,9 +4,25 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet {
 
-    public void settings() {
-        size(500, 500);                
+    public float map1(float from, float start1, float stop1, float start2, float stop2){
+
+        return 0;
     }
+    public void settings() {
+        size(500, 500);         
+        float f = map(2,0,10,0,width); // 2 is 20% of width
+        println(f);// should prints 100       
+        
+        f = map1(9,0,1,0,10);
+        println(f); // this will print 90
+
+        f = map1(250,200,300,400,500); //mapping the range
+        println(f); // this should print 450 - 50% of 400 and 500
+
+        f = map1(5,0,10,1000,2000); // 5 is halfway between 10
+        println(f); // this will print 1500 - will half of 1000 and 2000
+    }
+
 
     int mode = 0;
 
