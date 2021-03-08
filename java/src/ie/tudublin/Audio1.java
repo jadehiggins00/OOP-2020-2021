2f7d8e2d204411a899a67036bc71ef8145d5c484
jadehiggins00 <jadehiggins2000@gmail.com>
2021-03-08 23:53:20 +0000

    almost there

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
@@ -7,65 +8,77 @@ import ddf.minim.Minim;
 import processing.core.PApplet;
 
 public class Audio1 extends PApplet {
-
-    Minim minim; // Connect to minim
-    AudioInput ai; // How to connect to mic
+    //make an object of type minim
+    
+    //connect us to the library
+    Minim minim; 
+    //connect us to the mic
+    AudioInput ai;
+    // samples 
+    AudioBuffer ab;
+    //connect to an mp3 file
     AudioPlayer ap;
-    AudioBuffer ab; // Samples
-
-    float[] lerpedBuffer;
+ 
 
     public void settings()
     {
         size(512, 512);
-    }
+    }//end method
 
-    float y = 200;
-    float lerpedY = y;
+ 
 
     public void setup()
     {
-        minim = new Minim(this);
-        ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        //ap = minim.loadFile("scale.wav", width);        
-        // ap.play();
-        ab = ai.mix; // Connect the buffer to mic
-        //ab = ap.mix; // Connect the buffer to the mp3 file
+        minim = new Minim(this); //constructor
+        //stereo -two channel, MONO - one channel, second arg - frame size, arg3 - sample rate, arg4 - size of every sample in bits
+        ai = minim.getLineIn(Minim.MONO, width, 44100, 16); 
+        // load an mp3 file
+        ap = minim.loadFile("heroplanet.mp3", width); // second parameter is framesize
+        // this will start streaming the mp3
+        ap.play();
+        //asign audio buffer from the microphone
+        ab = ai.mix; // connects buffer to the mic
+        ab = ap.mix; // connects buffer to the mp3 file
         colorMode(HSB);
-        lerpedBuffer = new float[width];
 
-    }
+    }//end method
 
-
-    float lerpedAverage = 0;
+    //  lerp ( a, b ,t) -  t: how much between a and b,
+    //lerp means linear interpulation between a and t
+    
+    
 
     public void draw()
     {
         background(0);
+        // strokeWeight(2);
         stroke(255);
-        float halfHeight = height / 2;
+        float halfHeight = height /2;
         float average = 0;
         float sum = 0;
-        for(int i = 0 ; i < ab.size(); i ++)
-        {
-            float c = map(i, 0, ab.size(), 0, 255);
-            stroke(c, 255, 255);
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
+        //iterate over the audio buffer
+        // audio buffer is an array list
+        for (int i =0 ; i < ab.size(); i++){
 
-            line(i, halfHeight - lerpedBuffer[i] * halfHeight * 4, halfHeight + lerpedBuffer[i] * halfHeight * 4, i);
-            //println(ab.get(i));
-            sum += abs(ab.get(i));
-        }
+            //colour
+            float c = map(i,0, ab.size(), 0, 255);
+            stroke(c,255,255);
+            // i as x value and value for the end of the line
+            // y value value at half of the screen and ending at hald way down the screen plusaudio buffer at position i
+            // line(i, halfHeight, i, halfHeight + ab.get(i) * halfHeight);
+            // println(ab.get(i));
+
+            line(i, halfHeight -   ab.get(i) * halfHeight,i,halfHeight + ab.get(i) * halfHeight );
+            sum += abs(ab.get(i)); // abs - absolute value
+        }//end for
         average = sum / (float) ab.size();
-        // Calculate the AVERAGE amplitude
-        lerpedAverage = lerp(lerpedAverage, average, 0.1f);
-        ellipse(width / 4, 100, average * 500, average * 500);
-        ellipse(width / 2, 100, 50 + (lerpedAverage * 500), 50 + (lerpedAverage * 500));
+        //calculate the average amplitude
 
+        ellipse(width /2, 100, average * 500, average * 500); // circle
         
-        ellipse(200, y, 30, 30);
-        ellipse(300, lerpedY, 30, 30);
-        y += random(-10, 10);
-        lerpedY = lerp(lerpedY, y, 0.1f);        
-    }   
-}
+
+    }//end method
+
+    
+
+}//end class
