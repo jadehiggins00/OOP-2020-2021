025c15c2d2b282d102df4669fb76f58abea2c5b9
jadehiggins00 <jadehiggins2000@gmail.com>
2021-03-23 15:29:41 +0000

    moving circle

--- java/src/ie/tudublin/ProcessingExamples.java
+++ java/src/ie/tudublin/ProcessingExamples.java
@@ -1,95 +1,37 @@
 package ie.tudublin;
+
 import processing.core.PApplet;
 
 public class ProcessingExamples extends PApplet {
-    
- 
 
+    float angle = 0.0f;
+    float offset = 60;
+    float scalar = 30;
+    float speed = 0.05f;
 
-    
     public void settings() {
-        
-        size(480,120);
-     
-      
 
-    }//end mthod
+        size(500, 500);
+
+    }// end mthod
+
+    public void moveCircle() {
+        float x = offset + cos(angle) * scalar;
+        float y = offset + sin(angle) * scalar;
+
+        fill(0, 255, 0);
+        ellipse(x * 2, y * 2, 40, 40);
+
+        // rect(x * 2, y * 2, 40, 40);
+        angle += speed;
+
+    }// end method
 
     public void draw() {
-        // drawing an arrow
-        // beginShape();
-        // fill(153,176,180);
-        // vertex(180,82);
-        // vertex(207, 36);
-        // vertex(214, 63);
-        // vertex(407, 11);
-        // vertex(411, 30);
-        // vertex(219, 82);
-        // vertex(226, 109);
-        // endShape(CLOSE); //closes the lines at the end
 
+        background(255);
+        moveCircle();
 
-        // drawing the neck of robot
-        strokeWeight(2);
-        // colorMode(RGB);
-        background(252, 186, 3);
-        ellipseMode(RADIUS);
+    }// end method
 
-
-        stroke(255); //white
-
-        //neck
-        line(255,257,255, 180); //first line
-        line(266,257, 266, 180); // x1,y1,x2,y2   y2 is top point
-        line(277,257,277,180);
-        line(288,257,288,180);
-
-        //attennae
-        line(296,113,316,56); // right attenna
-        line(246,125,195,75); // x2 top point left atenna
-
-        //body
-        noStroke();
-        fill(126, 5, 255); // set fill to purple
-        rect(219, 257, 90, 110); //main body // x, y, width, height
-        fill(91, 255, 3); // set fill to green for stripes
-        rect(219, 270, 90, 6); // stripe 1
-        rect(219, 300, 90, 6); // stripe 2
-        rect(219, 330, 90, 6); // stripe 3
-
-        //head 
-        fill(126, 5, 255); //purple
-        ellipse(276, 155, 45,45); // x, y, width,height
-
-        // colorMode(HSB);
-        // strokeWeight(2);
-        // int numLines = (int)(mouseX / 10.0f);
-        // float cgap = 255 / (float) numLines;
-        // for( int i = 20 ; i < 400; i+=20) {
-        //     // stroke(cgap*i, 255,255);
-        //     line(i, 0, i+ i/2, 80); //spanning out the lines 
-        // }
-
-        // embedded one for loop in another
-        // background(0);
-        // noStroke();
-        // for (int y = 0 ;  y <= height ; y+=40) {
-
-        //     for(int x = 0 ; x <= width ; x +=40){
-        //         fill(255,140);
-        //         // ellipse(x,y,40,40);
-        //         rect(x,y, 40, 40);
-
-        //     }//end outer loop
-
-
-        // }//end outer loop
-       
-        
-
-    }
-    
-  
-
-
-}//end class
+}// end class
