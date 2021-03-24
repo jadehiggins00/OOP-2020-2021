30065908e466ca7b8dbf03cf84ceed22bff39e94
jadehiggins00 <jadehiggins2000@gmail.com>
2021-03-24 16:11:25 +0000

    1st version

---
+++ java/src/ie/tudublin/Health.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+
+public class Health {
+    float x, y;
+    float dx, dy;
+    YASC yasc; // referencing the yasc cl
+
+    public void Health(float x, float y, float dx, float dy){
+
+
+    }
+
+
+
+}//end class
