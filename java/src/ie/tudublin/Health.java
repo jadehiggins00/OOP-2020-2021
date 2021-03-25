4276fe16ba635e46fd2aa4761903cc02332a2345
jadehiggins00 <jadehiggins2000@gmail.com>
2021-03-25 09:02:44 +0000

    almost there

--- java/src/ie/tudublin/Health.java
+++ java/src/ie/tudublin/Health.java
@@ -1,68 +1,64 @@
 package ie.tudublin;
+
 import processing.core.PApplet;
 
 public class Health {
     float x, y;
     float dx, dy;
     float w = 50;
-    
-    float halfW = w /2;
+
+    float halfW = w / 2;
     YASC yasc; // referencing the yasc cl
     float rotation;
     float ranX, ranY;
-    
 
-    //constructor 
-    public Health(YASC yasc, float x, float y){
+    // constructor
+    public Health(YASC yasc, float x, float y) {
         this.yasc = yasc;
         this.x = x;
         this.y = y;
         // for random position on the screen
-        ranX = yasc.random(yasc.width -70);
-        ranY = yasc.random(yasc.height -80);
+        ranX = yasc.random(yasc.width - 270);
+        ranY = yasc.random(yasc.height - 80);
 
         rotation = 0;
 
-    }//end constructor
-    
+    }// end constructor
+
     // drawing the health object
-    void render(float x, float y){
-        yasc.stroke(0,255,0);
+    void render(float x, float y) {
+        yasc.stroke(0, 255, 0);
         yasc.noFill();
         yasc.translate(x, y);
         yasc.rotate(rotation);
         // yasc.rect(w,halfW,40,40);
-        yasc.line(- halfW, halfW, 0, -halfW);
-        yasc.line(0, - halfW, halfW, halfW);
+        yasc.line(-halfW, halfW, 0, -halfW);
+        yasc.line(0, -halfW, halfW, halfW);
         yasc.line(halfW, halfW, 0, 0);
-        yasc.line(0, 0, - halfW, halfW);
+        yasc.line(0, 0, -halfW, halfW);
 
-    }//end method
+    }// end method
 
     // moving the object
-    void update(){
+    void update() {
         dx = PApplet.sin(rotation);
-        dy =  - PApplet.cos(rotation);
+        dy = -PApplet.cos(rotation);
 
-        // float speed = 2.5f;
+        float speed = 2.5f;
         // x += yasc.random(-speed, speed);
-        // y += yasc.random(-speed, speed);
+        y += yasc.random(-speed, speed);
 
         // diagonal direction
-        yasc.random(dx,dy);
+
         x += dx;
         y += dy;
         x++;
-        
 
         // rotate
         yasc.rotate(rotation);
         rotation -= 0.05f;
-        
 
-
-
-    }//endmethod
+    }// endmethod
 
     public float getX() {
         return x;
@@ -79,8 +75,5 @@ public class Health {
     public void setY(float y) {
         this.y = y;
     }
-    
 
-
-
-}//end class
+}// end class
